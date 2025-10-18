32ddd7bbe855425fb39ab095f76da73b680fc186
yuta <okazaki.yuta.q1@dc.tohoku.ac.jp>
2025-10-18 22:20:27 +0900

    ユーザー情報の受け渡しの追加

--- llm/gemini_planner.py
+++ llm/gemini_planner.py
@@ -17,11 +17,12 @@ class GeminiPlanner:
         self.client = genai.Client()
 
     # 目標と期限を与えて、コメントとタスクのリストを取得
-    def ask(self, text: str, deadline: str) -> Response:
+    def ask(self, text: str, deadline: str, department: str, major: str, proficiency: int) -> Response:
         contents = (
             f"今日の日付は{datetime.date.today().isoformat()}です。"
+            f"ユーザーは以下の属性を持っています。\n学部:　{department}\n専攻: {major}\n習熟度（1-5）: {proficiency}\n"
             f"ユーザーは以下の目標と達成期限を設定しました。"
-            f"コメントとともに、期限内に目標を達成するためのマイルストーンとなるタスクをいくつか提案してください。"
+            f"コメントとともに、期限内に目標を達成するためのマイルストーンとなるタスクを{department}の学生が{text}を達成するために必要なことを調べて、ユーザーの学部、専攻、習熟度を重視して提案してください。"
             f"各タスクには必ず 'details'（日本語で100字以内の説明）を含めてください。"
             f"\n目標: {text}\n期限: {deadline}\nタスクは名前（name）、期限（deadline）、詳細（details）を含むリストとして提供してください。"
         )
@@ -40,5 +41,5 @@ class GeminiPlanner:
 if __name__ == "__main__":
     # テストコード
     planner = GeminiPlanner()
-    response = planner.ask("卒論を完成させたいです", "2026-03-31")
+    response = planner.ask("卒論を完成させたいです", "2026-03-31", "工学部", "情報工学科", 3)
     print(response)
