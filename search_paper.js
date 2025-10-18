ca309b831298188f0ee0e3ddb47accd7aa5036ee
yuna820 <yunana20020820@gmail.com>
2025-10-18 20:20:20 +0900

    send json data

--- search_paper.js
+++ search_paper.js
@@ -1,5 +1,6 @@
 const add_button = document.getElementById('add_button');
 const delete_button = document.getElementById('delete_button');
+const submit_button = document.getElementById('submit_button');
 
 function addForm() {
     var template = document.getElementById('input_form');
@@ -27,5 +28,21 @@ function deleteForm() {
     }
 }
 
+function submitForm() {
+    let submit_data = {};
+    var keywords = [];
+    var keyword_inputs = document.querySelectorAll('#keyword_entry input[name="keyword"]');
+    var logic = document.querySelector('input[name="logic"]:checked').value;
+
+    keyword_inputs.forEach(function(input) {
+        keywords.push(input.value);
+    });
+    submit_data["keywords"] = keywords;
+    submit_data["logic"] = logic;
+
+    document.keywords.submit();
+}
+
 add_button.addEventListener('click', addForm);
 delete_button.addEventListener('click', deleteForm);
+submit_button.addEventListener('click', submitForm);
