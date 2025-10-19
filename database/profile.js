54cceca9d6ed2fd9a03e376f48df75274beff29c
tungsten_405 <zhenwujiateng52@gmail.com>
2025-10-19 14:15:29 +0900

    不要なファイル, ディレクトリを削除

--- database/profile.js
+++
@@ -1,4 +0,0 @@
-[
-  {"user_id" : "labnow1", "password" : "password1", "department" : "工学", "major": "通信工学", "level": "2"},
-  {"user_id" : "labnow2", "password" : "password2", "department" : "理学", "major": "物理学", "level": "4"}
-]
